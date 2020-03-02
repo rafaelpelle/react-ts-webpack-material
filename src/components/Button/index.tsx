5228997208c7a6d541ed4221c8702dc034d2530b
Rafael Pelle <rafael.pelle@supero.com.br>
2020-03-02 15:43:38 -0300

    Criar button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,61 @@
+import * as React from 'react'
+import MaterialButton from '@material-ui/core/Button'
+import CircularProgress from '@material-ui/core/CircularProgress'
+
+const Loader: React.FC<Props> = (props) => {
+	const { color, variant, size } = props
+	const thickness = size === 'small' ? 5 : size === 'large' ? 7 : 6
+	const circularSize = size === 'small' ? 20 : size === 'large' ? 29 : 24
+	const circularColor = color === 'primary' && variant === 'contained' ? 'secondary' : 'primary'
+	return (
+		<CircularProgress size={ circularSize } thickness={ thickness } color={ circularColor } style={ { margin: '0 auto' } } />
+	)
+}
+
+const Button: React.FC<Props> = (props) => (
+	<MaterialButton
+		color={ props.color }
+		size={ props.size }
+		variant={ props.variant }
+		fullWidth={ props.fullWidth }
+		disabled={ props.disabled }
+		style={ props.style }
+		onClick={ props.onClick }
+		disableRipple={ props.disableRipple }
+		disableFocusRipple
+	>
+		{ !props.loading ? props.content : <Loader { ...props } /> }
+	</MaterialButton>
+)
+export default Button
+
+/////////////////////////////////////////////////////////////////
+///////////////////////////// STYLES ////////////////////////////
+/////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+/////////////////////////// INTERFACES //////////////////////////
+/////////////////////////////////////////////////////////////////
+type Color = 'inherit' | 'primary' | 'secondary' | 'default'
+type Size = 'small' | 'medium' | 'large'
+type Variant = 'text' | 'outlined' | 'contained'
+
+interface OwnProps {
+	content?: string
+	color?: Color
+	size?: Size
+	variant?: Variant
+	loading?: boolean
+	disabled?: boolean
+	disableRipple?: boolean
+	disableFocusRipple?: boolean
+	fullWidth?: boolean
+	style?: any
+	onClick?: (...args: any) => any
+}
+
+interface StateProps {}
+
+interface DispatchProps {}
+
+type Props = StateProps & DispatchProps & OwnProps
--- src/pages/Page1/index.tsx
+++ src/pages/Page1/index.tsx
@@ -1,20 +1,15 @@
 import * as React from 'react'
-import {history} from '../../router/history'
+import { history } from '../../router/history'
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
 
 const Page1: React.FC<Props> = (props) => {
 	return (
-		<div style={{textAlign: 'center'}}>
-			<Typography variant='h3' align='center' style={textStyle}>
+		<div style={ { textAlign: 'center' } }>
+			<Typography variant='h3' align='center' style={ textStyle }>
 				Page1
 			</Typography>
-			<Button
-				color='primary'
-				variant='contained'
-				style={buttonStyle}
-				onClick={() => history.push('/page2')}
-			>
+			<Button color='primary' variant='contained' style={ buttonStyle } onClick={ () => history.push('/page2') }>
 				Lazy load Page2
 			</Button>
 		</div>
