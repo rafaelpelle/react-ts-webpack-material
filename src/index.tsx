349f7b0adf935cea6061e760a6c5c3ce73f78038
Nikolas Damian <nikolas.vasconcelos@supero.com.br>
2020-03-02 16:21:10 -0300

    botão atualizado

--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,37 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import * as promise from 'redux-promise'
-import {Provider} from 'react-redux'
-import {composeWithDevTools} from 'redux-devtools-extension'
-import {applyMiddleware, createStore} from 'redux'
+import { Provider } from 'react-redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { applyMiddleware, createStore } from 'redux'
 import thunk from 'redux-thunk'
 import reducers from './redux/reducers/reducers'
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles'
 import RegisterSW from './utils/registerSW'
 import App from './main/app'
-import {appTheme} from './utils/theme'
-import {SnackbarProvider} from 'notistack'
+import { appTheme } from './utils/theme'
+import { SnackbarProvider } from 'notistack'
 
 const composeEnhancers = composeWithDevTools({
-	// options like actionSanitizer, stateSanitizer
+  // options like actionSanitizer, stateSanitizer
 })
 
 const composedWithMiddleware = composeEnhancers(
-	applyMiddleware(thunk, promise)
-	// other store enhancers if any
+  applyMiddleware(thunk, promise)
+  // other store enhancers if any
 )
 
 const store = createStore(reducers, composedWithMiddleware)
 
 ReactDOM.render(
-	<Provider store={store as any}>
-		<MuiThemeProvider theme={appTheme}>
-			<SnackbarProvider maxSnack={3} autoHideDuration={3000}>
-				<App />
-			</SnackbarProvider>
-		</MuiThemeProvider>
-	</Provider>,
-	document.getElementById('root')
+  <Provider store={store as any}>
+    <MuiThemeProvider theme={appTheme}>
+      <SnackbarProvider maxSnack={3} autoHideDuration={3000}>
+        <App />
+      </SnackbarProvider>
+    </MuiThemeProvider>
+  </Provider>,
+  document.getElementById('root')
 )
 
 RegisterSW()
--- src/pages/Page1/index.tsx
+++ src/pages/Page1/index.tsx
@@ -1,19 +1,31 @@
 import * as React from 'react'
 import { history } from '../../router/history'
-import Button from '@material-ui/core/Button'
+import Button from '../../components/Button'
 import Typography from '@material-ui/core/Typography'
 
 const Page1: React.FC<Props> = (props) => {
-	return (
-		<div style={ { textAlign: 'center' } }>
-			<Typography variant='h3' align='center' style={ textStyle }>
-				Page1
-			</Typography>
-			<Button color='primary' variant='contained' style={ buttonStyle } onClick={ () => history.push('/page2') }>
-				Lazy load Page2
-			</Button>
-		</div>
-	)
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <Typography variant='h3' align='center' style={textStyle}>
+        Page1
+      </Typography>
+      <Button
+        content='Cancelar'
+        style={buttonStyle}
+        variant='outlined'
+        color='secondary'
+        //onClick={() => history.push('/page2')}
+      />
+      <Button
+        content='Salvar'
+        style={buttonStyle}
+        variant='contained'
+        color='secondary'
+        loading={false}
+        //onClick={() => history.push('/page2')}
+      />
+    </div>
+  )
 }
 export default Page1
 
@@ -21,10 +33,10 @@ export default Page1
 /////////////////////////////////// STYLES ///////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
 const textStyle = {
-	marginTop: '30vh',
+  marginTop: '30vh',
 }
 const buttonStyle = {
-	marginTop: '50px',
+  marginTop: '50px',
 }
 //////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////// INTERFACES /////////////////////////////////
