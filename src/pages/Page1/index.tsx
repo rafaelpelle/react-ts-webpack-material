f711975b8a20a3fa1267c16a4e879bd145f0f9c3
Rafael Pelle <rafael.pelle@supero.com.br>
2020-03-02 17:09:38 -0300

    Updating libs

--- src/pages/Page1/index.tsx
+++ src/pages/Page1/index.tsx
@@ -4,28 +4,20 @@ import Button from '../../components/Button'
 import Typography from '@material-ui/core/Typography'
 
 const Page1: React.FC<Props> = (props) => {
-  return (
-    <div style={{ textAlign: 'center' }}>
-      <Typography variant='h3' align='center' style={textStyle}>
-        Page1
-      </Typography>
-      <Button
-        content='Cancelar'
-        style={buttonStyle}
-        variant='outlined'
-        color='secondary'
-        //onClick={() => history.push('/page2')}
-      />
-      <Button
-        content='Salvar'
-        style={buttonStyle}
-        variant='contained'
-        color='secondary'
-        loading={false}
-        //onClick={() => history.push('/page2')}
-      />
-    </div>
-  )
+	return (
+		<div style={ { textAlign: 'center' } }>
+			<Typography variant='h3' align='center' style={ textStyle }>
+				Page1
+			</Typography>
+			<Button
+				content='Lazy Load Page 2'
+				style={ buttonStyle }
+				variant='contained'
+				color='primary'
+				onClick={ () => history.push('/page2') }
+			/>
+		</div>
+	)
 }
 export default Page1
 
@@ -33,10 +25,10 @@ export default Page1
 /////////////////////////////////// STYLES ///////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
 const textStyle = {
-  marginTop: '30vh',
+	marginTop: '30vh',
 }
 const buttonStyle = {
-  marginTop: '50px',
+	marginTop: '50px',
 }
 //////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////// INTERFACES /////////////////////////////////
